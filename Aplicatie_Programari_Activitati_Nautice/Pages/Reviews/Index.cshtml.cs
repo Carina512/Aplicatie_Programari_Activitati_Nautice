2097f430f6b8a8158945fbd0caf5e3bc07beeef6
Carina <carina_cary2001@yahoo.com>
2024-02-14 23:29:30 +0200

    cu login

--- Aplicatie_Programari_Activitati_Nautice/Pages/Reviews/Index.cshtml.cs
+++ Aplicatie_Programari_Activitati_Nautice/Pages/Reviews/Index.cshtml.cs
@@ -28,5 +28,22 @@ namespace Aplicatie_Programari_Activitati_Nautice.Pages.Reviews
                 Review = await _context.Review.ToListAsync();
             }
         }
+        public string GetStarRating(int nota)
+        {
+            string stelutePline = string.Empty;
+            string steluteGoale = string.Empty;
+
+            for (int i = 0; i < nota; i++)
+            {
+                stelutePline += "★"; // Adăugați un caracter stea plină
+            }
+
+            for (int i = nota; i < 5; i++)
+            {
+                steluteGoale += "☆"; // Adăugați un caracter stea goală
+            }
+
+            return stelutePline + steluteGoale;
+        }
     }
 }
--- Aplicatie_Programari_Activitati_Nautice/Program.cs
+++ Aplicatie_Programari_Activitati_Nautice/Program.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Aplicatie_Programari_Activitati_Nautice.Data;
+using Microsoft.AspNetCore.Identity;
+using Aplicatie_Programari_Activitati_Nautice.Areas.Identity.Data;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<Aplicatie_Programari_Activitati_NauticeContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Aplicatie_Programari_Activitati_NauticeContext") ?? throw new InvalidOperationException("Connection string 'Aplicatie_Programari_Activitati_NauticeContext' not found.")));
+builder.Services.AddDbContext<Aplicatie_Programari_Activitati_NauticeContext>((options =>
+
+options.UseSqlServer(builder.Configuration.GetConnectionString("Aplicatie_Programari_Activitati_NauticeContext") ?? throw new InvalidOperationException("Connection string 'Aplicatie_Programari_Activitati_NauticeContext' not found."))));
+builder.Services.AddDbContext<LibraryIdentityContext>(options =>
+
+options.UseSqlServer(builder.Configuration.GetConnectionString("Aplicatie_Programari_Activitati_NauticeContext") ?? throw new InvalidOperationException("Connectionstring 'Aplicatie_Programari_Activitati_NauticeContext' not found.")));
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+options.SignIn.RequireConfirmedAccount = true)
+  .AddRoles<IdentityRole>()
+ .AddEntityFrameworkStores<LibraryIdentityContext>();
 
 var app = builder.Build();
 
@@ -22,9 +34,43 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    var roles = new[] { "Admin", "User" };
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+            await roleManager.CreateAsync(new IdentityRole(role));
+    }
+}
+using (var scope = app.Services.CreateScope())
+{
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+    string email = "admin@admin.com";
+    string password = "Admin1234!";
+
+    if (await userManager.FindByEmailAsync(email) == null)
+    {
+        var user = new IdentityUser();
+        user.UserName = email;
+        user.Email = email;
+        user.EmailConfirmed = true;
+
+        await userManager.CreateAsync(user, password);
+
+        await userManager.AddToRoleAsync(user, "Admin");
+
+    }
+
+}
 
 app.Run();
